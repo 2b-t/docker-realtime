64f38c330159c7104c9b2d2e41264297be88b2a0
2b-t <t.flatscher@hotmail.de>
2022-04-03 00:22:34 +0200

    feat: Updated PREEMPT_RT patch script

--- patch_kernel_preemptrt.sh
+++ patch_kernel_preemptrt.sh
@@ -6,10 +6,9 @@
 # Install required dependencies
 function install_dependencies {
   echo "Installing dependencies..."
-  sudo apt-get install -y dialog
   sudo apt-get install -y grep curl
-  sudo apt-get install -y fakeroot kernel-package linux-source libncurses-dev libssl-dev equivs gcc flex bison dpkg-dev
-  echo "Dependencies installed successfully"
+  sudo apt-get install -y build-essential bc ca-certificates gnupg2 libssl-dev lsb-release libelf-dev bison flex dwarves zstd libncurses-dev fakeroot kernel-package linux-source equivs gcc dpkg-dev
+  echo "Dependencies installed successfully!"
 }
 
 # Get major versions by crawling website
@@ -54,34 +53,75 @@ function reconstruct_kernel_major_version {
 # Download and extract the vanilla kernel
 function download_and_extract_kernel {
   echo "Downloading kernel '${KERNEL_VER_FULL}'..."
-  echo "https://www.kernel.org/pub/linux/kernel/${KERNEL_VER}/linux-${KERNEL_VER_FULL}.tar.xz"
-  echo "https://www.kernel.org/pub/linux/kernel/${KERNEL_VER}/linux-${KERNEL_VER_FULL}.tar.sign"
   curl -SLO --fail https://www.kernel.org/pub/linux/kernel/${KERNEL_VER}/linux-${KERNEL_VER_FULL}.tar.xz
   curl -SLO --fail https://www.kernel.org/pub/linux/kernel/${KERNEL_VER}/linux-${KERNEL_VER_FULL}.tar.sign
   xz -d linux-${KERNEL_VER_FULL}.tar.xz
-  tar xf linux-${KERNEL_VER_FULL}.tar
 }
 
 # Download and extract the PREEMPT_RT patch
 function download_and_extract_preemptrt {
   echo "Downloading PREEMPT_RT patch '${PREEMPT_RT_VER_FULL}'..."
-  echo "https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/${PREEMPT_RT_VER}/patch-${PREEMPT_RT_VER_FULL}.patch.xz"
-  echo "https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/${PREEMPT_RT_VER}/patch-${PREEMPT_RT_VER_FULL}.patch.sign"
   curl -SLO --fail https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/${PREEMPT_RT_VER}/patch-${PREEMPT_RT_VER_FULL}.patch.xz
   curl -SLO --fail https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/${PREEMPT_RT_VER}/patch-${PREEMPT_RT_VER_FULL}.patch.sign 
   xz -d patch-${PREEMPT_RT_VER_FULL}.patch.xz
 }
 
+# Sign the kernel and the patch
+function sign_kernel_and_preemptrt {
+  echo "Signing keys..."
+  gpg2 --verify linux-${KERNEL_VER_FULL}.tar.sign
+  if [ $? -ne 0 ]
+    then
+      local KERNEL_KEY=$(gpg2 --verify linux-${KERNEL_VER_FULL}.tar.sign 2>&1 | grep -o -P '(?<=RSA key )(.*)')
+      gpg2 --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $KERNEL_KEY
+      gpg2 --verify linux-${KERNEL_VER_FULL}.tar.sign
+  fi
+  
+  gpg2 --verify patch-${PREEMPT_RT_VER_FULL}.patch.sign
+  if [ $? -ne 0 ]
+    then
+      local PREEMPT_KEY=$(gpg2 --verify patch-${PREEMPT_RT_VER_FULL}.patch.sign 2>&1 | grep -o -P '(?<=RSA key )(.*)')
+      gpg2 --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $PREEMPT_KEY
+      gpg2 --verify patch-${PREEMPT_RT_VER_FULL}.patch.sign
+  fi
+}
+
 # Apply the patch to the kernel
 function apply_patch_to_kernel {
   echo "Applying PREEMPT_RT patch '${PREEMPT_RT_VER_FULL}' to kernel '${KERNEL_VER_FULL}'..."
+  tar xf linux-${KERNEL_VER_FULL}.tar
   cd linux-${KERNEL_VER_FULL}
   patch -p1 < ../patch-${PREEMPT_RT_VER_FULL}.patch
 }
 
 # Generate a new kernel configuration
 function generate_new_kernel_configuration {
-  make oldconfig
+  # Only option set in the configuration is full PREEMPT_RT, rest is left on default
+  echo "Generating kernel configuration"
+  echo "4" | make oldconfig
+}
+
+# Function for finding a certain setting, replacing it or adding it if not existing
+# @param $1 The name of the setting
+# @param $2 its desired value
+function find_and_replace_in_config {
+  local CONFIG_FILE=".config"
+  {
+    sed -i "s/$1=.*/$1=$2/" $CONFIG_FILE;
+  } ||
+  {
+    echo $1"="$2 >> $CONFIG_FILE
+  }
+}
+
+# Unsign the kernel configuration
+function unsign_kernel_configuration {
+  echo "Forcing unsigned kernel..."
+  find_and_replace_in_config "CONFIG_MODULE_SIG" "n"
+  find_and_replace_in_config "CONFIG_MODULE_SIG_ALL" "n"
+  find_and_replace_in_config "CONFIG_MODULE_SIG_FORCE" "n"
+  find_and_replace_in_config "CONFIG_MODULE_SIG_KEY" '""'
+  find_and_replace_in_config "CONFIG_SYSTEM_TRUSTED_KEYS" '""'
 }
 
 # Select the installation modality
@@ -90,16 +130,16 @@ function select_installation_mode {
 }
 
 # Generate a debian package for easier installation
-function generate_preemptrt_debian_package {
+function generate_preemptrt_kernel_debian_package {
   echo "Generating Debian package..."
   make-kpkg clean
   fakeroot make-kpkg -j$(nproc) --initrd --revision=1.0.custom kernel_image
 }
 
 # Install PREEMPT_RT from the Debian package
-function install_preemptrt_kernel_as_debian_package {
+function install_preemptrt_kernel_debian_package {
   echo "Installing Debian package..."
-  sudo dpkg -i ../linux-image-${PREEMPT_RT_VER_FULL}_1.0.custom_amd64.deb
+  sudo dpkg -i ../linux-image-${PREEMPT_RT_VER_FULL}_1.0.custom_$(dpkg --print-architecture).deb
 }
 
 # Install the kernel directly without creating a Debian package first
@@ -110,9 +150,10 @@ function install_preemptrt_kernel_directly {
   sudo make install -j$(nproc)
 }
 
-
-function main {
+# Install and let the user decide the kernel etc.
+function install_kernel_interactive {
   {
+    sudo apt-get install -y dialog
     install_dependencies
   } || {
     echo "Warning: Could not install dependencies. Installation might fail!"
@@ -124,9 +165,10 @@ function main {
   local KERNEL_VER=$(reconstruct_kernel_major_version) # e.g. v5.x
   local KERNEL_VER_FULL=$(echo "${PREEMPT_RT_VER_FULL}" | sed 's/-rt.*//g') # e.g. 5.10.78
 
-  # Download and extract the files
+  # Download and extract the files for kernel and patch
   download_and_extract_kernel
   download_and_extract_preemptrt
+  sign_kernel_and_preemptrt
 
   # Apply patch to kernel
   apply_patch_to_kernel
@@ -135,12 +177,53 @@ function main {
   # Choose between Debian package and installing directly
   local INSTALLATION_MODE=$(select_installation_mode)
   if [ "$INSTALLATION_MODE" == "Debian" ]; then
-    generate_preemptrt_debian_package
-    install_preemptrt_kernel_as_debian_package
+    generate_preemptrt_kernel_debian_package
+    install_preemptrt_kernel_debian_package
   else
     install_preemptrt_kernel_directly
   fi
+}
 
+# Installs the kernel as Debian package from a given command-line argument without interactive choices
+function install_kernel_noninteractive {
+  {
+    install_dependencies
+  } || {
+    echo "Warning: Could not install dependencies. Installation might fail!"
+  }
+
+  local PREEMPT_RT_VER_FULL=$1 # e.g. 5.10.78-rt55
+  local KERNEL_VER_FULL=$(echo "${PREEMPT_RT_VER_FULL}" | sed 's/-rt.*//g') # e.g. 5.10.78
+  local PREEMPT_RT_VER=$(echo "${KERNEL_VER_FULL}" | sed 's/\.[^\.]*//2g') # e.g. 5.10
+  local KERNEL_VER="v"$(echo "${PREEMPT_RT_VER_FULL}" | sed -n 's/\(.*\)[.]\(.*\)/\1.x/p') #e.g. v5.x
+
+  download_and_extract_kernel
+  download_and_extract_preemptrt
+  sign_kernel_and_preemptrt
+  apply_patch_to_kernel
+  generate_new_kernel_configuration
+  generate_preemptrt_kernel_debian_package
+  install_preemptrt_kernel_debian_package
+}
+
+
+# Install the kernel either in an interactive or non-interactive way depending if an input argument is given
+function main {
+  if [ "$#" -eq 0 ]
+    then
+      install_kernel_interactive
+  elif [ "$#" -eq 1 ] && [ "${EUID}" -eq 0 ]
+    then
+      install_kernel_noninteractive $@
+  else
+    echo "Error: Could not run installation script!"
+    echo "Either launch it as: "
+    echo " - '$ ./patch_kernel_preemprt.sh'"
+    echo " - '$ sudo ./patch_kernel_preemprt.sh 5.10.78-rt55'"
+    echo "For the available PREEMPT_RT versions see: "
+    echo "see https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/"
+    exit 1
+  fi
   exit 0
 }
 
